595f40d7b4206b93b732a5e6964b8d972a27453c
nathanradavies <84192926+nathanradavies@users.noreply.github.com>
2021-11-08 13:25:45 -0600

    Update install.sh
    Corrected download link

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 cd /lib/firmware/brcm
 wget https://github.com/RPi-Distro/firmware-nonfree/raw/b7e706b67d1e16a939b39c4fe46acc446c060fcc/brcm/brcmfmac43456-sdio.bin
 wget https://github.com/RPi-Distro/firmware-nonfree/raw/b7e706b67d1e16a939b39c4fe46acc446c060fcc/brcm/brcmfmac43456-sdio.clm_blob
-wget https://github.com/RPi-Distro/firmware-nonfree/raw/b7e706b67d1e16a939b39c4fe46acc446c060fcc/brcm/brcmfmac43456-sdio.clm_blob
+wget https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/b7e706b67d1e16a939b39c4fe46acc446c060fcc/brcm/brcmfmac43456-sdio.txt
