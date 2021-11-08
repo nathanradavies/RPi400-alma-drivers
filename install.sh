d40233476dba949c4070d3f79f072c44f8348ecc
nathanradavies <84192926+nathanradavies@users.noreply.github.com>
2021-11-08 13:17:19 -0600

    Create install.sh
    Added setup for drivers

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /lib/firmware/brcm
+wget https://github.com/RPi-Distro/firmware-nonfree/raw/b7e706b67d1e16a939b39c4fe46acc446c060fcc/brcm/brcmfmac43456-sdio.bin
+wget https://github.com/RPi-Distro/firmware-nonfree/raw/b7e706b67d1e16a939b39c4fe46acc446c060fcc/brcm/brcmfmac43456-sdio.clm_blob
+wget https://github.com/RPi-Distro/firmware-nonfree/raw/b7e706b67d1e16a939b39c4fe46acc446c060fcc/brcm/brcmfmac43456-sdio.clm_blob
